3fd072e9f98a6f36e94640fda43461045c275210
Jordao <jordao@ymail.com>
2011-05-18 15:56:21 -0400

    Corrected small documentation error

--- src/NRoles.Engine/Support/AssemblyVerifier.cs
+++ src/NRoles.Engine/Support/AssemblyVerifier.cs
@@ -36,7 +36,7 @@ namespace NRoles.Engine {
     /// Creates a new instance of this class.
     /// </summary>
     /// <param name="assembly">Assembly to verify.</param>
-    /// <param name="timeoutInMillis">Timeout for the verification process in milliseconds. Valid values are from 5000 to 60000.</param>
+    /// <param name="timeoutInMillis">Timeout for the verification process in milliseconds. Valid values are from 1000 to 60000.</param>
     public AssemblyVerifier(AssemblyDefinition assembly, int timeoutInMillis = 5000) {
       if (assembly == null) throw new ArgumentNullException("assembly");
       if (timeoutInMillis < 1000 || timeoutInMillis > 60000) throw new ArgumentOutOfRangeException("timeoutInMillis", "timeout must be between 1 annd 60s (1000 and 60000ms)");
@@ -49,7 +49,7 @@ namespace NRoles.Engine {
     /// Creates a new instance of this class.
     /// </summary>
     /// <param name="assemblyPath">Path to the assembly to verify.</param>
-    /// <param name="timeoutInMillis">Timeout for the verification process in milliseconds. Valid values are from 5000 to 60000.</param>
+    /// <param name="timeoutInMillis">Timeout for the verification process in milliseconds. Valid values are from 1000 to 60000.</param>
     public AssemblyVerifier(string assemblyPath, int timeoutInMillis = 5000) {
       if (assemblyPath == null) throw new ArgumentNullException("assemblyPath");
       if (timeoutInMillis < 1000 || timeoutInMillis > 60000) throw new ArgumentOutOfRangeException("timeoutInMillis", "timeout must be between 1 annd 60s (1000 and 60000ms)");
