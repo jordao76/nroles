37dd44aefa2eb42f06cd5e644a90795040a7a355
Jordao <jordao@ymail.com>
2011-05-20 18:56:37 -0400

    Generic class was not changing calls to callvirt; fixed now

--- src/NRoles.Engine/CodeVisitors/ChangeCallToCallVirtVisitor.cs
+++ src/NRoles.Engine/CodeVisitors/ChangeCallToCallVirtVisitor.cs
@@ -8,28 +8,22 @@ using Mono.Cecil.Cil;
 namespace NRoles.Engine {
 
   class ChangeCallToCallVirtVisitor : CodeVisitorBase {
-
     private TypeDefinition _sourceType;
-    
     public ChangeCallToCallVirtVisitor(TypeDefinition sourceType) {
       _sourceType = sourceType;
     }
-
     public override void VisitInstruction(Instruction instruction) {
-      var method = instruction.Operand as MethodReference;
-      if (method != null && ShouldChangeToCallVirt(method)) {
-        if (instruction.OpCode == OpCodes.Call) {
-          instruction.OpCode = OpCodes.Callvirt;
-        }
+      if (instruction.OpCode == OpCodes.Call && (instruction.Operand is MethodReference) && ShouldChangeToCallVirt((MethodReference)instruction.Operand)) {
+        instruction.OpCode = OpCodes.Callvirt;
       }
     }
-
     protected bool ShouldChangeToCallVirt(MethodReference method) {
+      var methodDefinition = method.Resolve();
       return
         method.DeclaringType.Resolve() == _sourceType &&
-        method.Resolve().RemainsInRoleInterface();
+        // private and static methods should not change to callvirt
+        !methodDefinition.IsPrivate && !methodDefinition.IsStatic;
     }
-
   }
 
 }
