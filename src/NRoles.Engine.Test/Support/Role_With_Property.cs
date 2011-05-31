8325ac4a3d1b655050de52407c4a22ab479d2379
Jordao <jordao@ymail.com>
2011-05-31 08:24:34 -0400

    Superceded property from base class problem fixed

--- src/NRoles.Engine.Test/Support/Role_With_Property.cs
+++ src/NRoles.Engine.Test/Support/Role_With_Property.cs
@@ -5,10 +5,16 @@ using System.Text;
 
 namespace NRoles.Engine.Test.Support {
 
-  //[RoleTest(CompositionType = typeof(Derived_Class_From_Base_Class_With_Property))]
+  [RoleTest(CompositionType = typeof(Role_With_Property_Composition_With_Property_Superceded))]
+  [RoleTest(CompositionType = typeof(Derived_Class_From_Base_Class_With_Property))]
   public interface Role_With_Property : Role {
     int Property { get; }
   }
+
+  public class Role_With_Property_Composition_With_Property_Superceded : Does<Role_With_Property> {
+    public int Property { get; set; }
+  }
+  
   public class Base_Class_With_Property {
     public int Property { get { return 42; } }
   }
