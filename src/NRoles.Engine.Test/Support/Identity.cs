dca418e8fad259a86ffe7b4ae91a70d8039dda49
Jordao <jordao@ymail.com>
2011-06-13 17:51:22 -0400

    Self-Type flows through role-composition with self-type

--- src/NRoles.Engine.Test/Support/Identity.cs
+++ src/NRoles.Engine.Test/Support/Identity.cs
@@ -23,4 +23,20 @@ namespace NRoles.Engine.Test.Support {
   public class Identity_Composition_With_Wrong_Type_Argument : Does<Identity<Identity_Composition>> {
   }
 
+  [RoleTest(
+    OtherRoles = new Type[] { typeof(Identity<>) },
+    CompositionType = typeof(Identity_Role<>),
+    OtherCompositions = new Type[] { typeof(Identity_Role_Composition<>) })]
+  public class Identity_Role<T> : Does<Identity<Identity_Role<T>>>, Role { }
+  public class Identity_Role_Composition<T> : Does<Identity_Role<T>> { }
+
+  // here, the self-type parameter flows with the new role
+  [RoleTest(
+    OtherRoles = new Type[] { typeof(Identity<>) },
+    CompositionType = typeof(Identity_Role_With_Self_Type<>),
+    OtherCompositions = new Type[] { typeof(Identity_Role_With_Self_Type_Composition) })]
+  public class Identity_Role_With_Self_Type<S> : Does<Identity<S>>, Role { }
+  public class Identity_Role_With_Self_Type_Composition : 
+    Does<Identity_Role_With_Self_Type<Identity_Role_With_Self_Type_Composition>> { }
+
 }
