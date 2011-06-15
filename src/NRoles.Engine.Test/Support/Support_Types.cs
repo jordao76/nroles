49aae494f2f61247acc080c07dbeb27d09cff068
Jordao <jordao@ymail.com>
2011-06-15 08:31:03 -0400

    Internal event in role

--- src/NRoles.Engine.Test/Support/Support_Types.cs
+++ src/NRoles.Engine.Test/Support/Support_Types.cs
@@ -107,63 +107,6 @@ namespace NRoles.Engine.Test.Support {
   public class Generic_Role_With_Method_Composition<U> : Does<Generic_Role_With_Method<U>> { }
   public class Generic_Role_With_Method_Composition_String : Does<Generic_Role_With_Method<string>> { }
 
-  [RoleTest(
-    CompositionType = typeof(Role_With_Event_Composition),
-    TestType = typeof(Role_With_Event_Test))]
-  public class Role_With_Event : Role {
-    public event EventHandler MyEvent = delegate { };
-    public void Operation() { FireEvent(); }
-    private void FireEvent() { MyEvent(this, EventArgs.Empty); }
-  }
-  public class Role_With_Event_Composition : Does<Role_With_Event> { }
-  public class Role_With_Event_Test : DynamicTestFixture {
-    public override void Test() {
-      var role = new Role_With_Event_Composition().As<Role_With_Event>();
-      string str = null;
-      var handler = (EventHandler)((s, e) => str = "Hello");
-      role.MyEvent += handler;
-      Assert.IsNull(str);
-      role.Operation();
-      Assert.AreEqual("Hello", str);
-      str = "Bye";
-      role.MyEvent -= handler;
-      role.Operation();
-      Assert.AreEqual("Bye", str);
-    }
-  }
-
-  public class EventArgs<T> : EventArgs {
-    public readonly T Element;
-    public EventArgs(T t) { Element = t; }
-  }
-
-  [RoleTest(
-    CompositionType = typeof(Generic_Role_With_Event_Composition<>),
-    TestType = typeof(Generic_Role_With_Event_Test))]
-  public class Generic_Role_With_Event<T> : Role {
-    public event EventHandler<EventArgs<T>> MyEvent = delegate { };
-    public void Operation(T element) { FireEvent(element); }
-    private void FireEvent(T element) { MyEvent(this, new EventArgs<T>(element)); }
-  }
-  public class Generic_Role_With_Event_Composition<T> : Does<Generic_Role_With_Event<T>> { }
-  public class Generic_Role_With_Event_Test : DynamicTestFixture {
-    public override void Test() {
-      var role = new Generic_Role_With_Event_Composition<int>().As<Generic_Role_With_Event<int>>();
-      string str = null;
-      var handler = (EventHandler<EventArgs<int>>)((s, e) => {
-        Assert.AreEqual(42, e.Element);
-        str = "Hello";
-      });
-      role.MyEvent += handler;
-      role.Operation(42);
-      Assert.AreEqual("Hello", str);
-      str = "Bye";
-      role.MyEvent -= handler;
-      role.Operation(42);
-      Assert.AreEqual("Bye", str);
-    }
-  }
-
   [RoleTest]
   public class Role_With_Private_Field : Role {
     private string _field = "Test";
