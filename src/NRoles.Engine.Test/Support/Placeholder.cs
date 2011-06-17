9a0a0e3fcd1c4f1d2b4569ebce4292e44a47b59f
Jordao <jordao@ymail.com>
2011-06-17 17:17:02 -0400

    Method placeholder - test file

---
+++ src/NRoles.Engine.Test/Support/Placeholder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NRoles.Engine.Test.Support {
+
+  [RoleTest(
+    CompositionType = typeof(DeepThought),
+    TestType = typeof(DeepThoughtTest))]
+
+  public class RDeepThought : Role {
+    public int Answer() { return 42; }
+  }
+
+  public class DeepThought : Does<RDeepThought> {
+    [Placeholder] public int Answer() { return 4 + 2; }
+  }
+
+  public class DeepThoughtTest : DynamicTestFixture {
+    public override void Test() {
+      var thinker = new DeepThought();
+      Assert.AreEqual(42, thinker.Answer());
+    }
+  }
+
+  // TODO: properties and events
+
+  // TODO: think about placeholders in roles that are also compositions
+
+  // TODO: placeholder in abstract method
+
+  // TODO: placeholder in an override of a base-class member
+
+}
