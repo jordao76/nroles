4b1bce859bce745e484b1e9857ee8b48a5b7e7a9
Jordao <jordao@ymail.com>
2011-06-20 08:24:27 -0400

    Improved acceptance tests infrastructure

---
+++ src/NRoles.Engine.Test/CompositionTestAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NRoles.Engine.Test {
+
+  [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = true)]
+  public class CompositionTestAttribute : MutationTestAttribute {
+    public override Type AnnotatedType {
+      get { return CompositionType; }
+      set { CompositionType = value; }
+    }
+    public override Type SupportingType {
+      get { return RoleType; }
+      set { RoleType = value; }
+    }
+  }
+
+}
---
+++ src/NRoles.Engine.Test/RoleTestAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NRoles.Engine.Test {
+
+  [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = true)]
+  public class RoleTestAttribute : MutationTestAttribute {
+    public override Type AnnotatedType {
+      get { return RoleType; }
+      set { RoleType = value; } 
+    }
+    public override Type SupportingType {
+      get { return CompositionType; }
+      set { CompositionType = value; }
+    }
+  }
+
+}
