3c3948be57fa3a2cc2e96ddae757518423477163
Jordao <jordao@ymail.com>
2011-06-20 17:27:17 -0400

    Placeholder in composition property replaced by concrete role property getter and setter

---
+++ src/NRoles.Engine.Test/Support/Placeholder_Properties.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NRoles.Engine.Test.Support.PlaceholderProperties {
+
+  [RoleTest(
+    Description = "Placeholder in composition property should be replaced by concrete role property getter and setter",
+    CompositionType = typeof(DeepThought),
+    TestType = typeof(DeepThoughtTest))]
+
+  public class RDeepThought : Role {
+    private int answer;
+    public int Answer {
+      get { return answer + 2; }
+      set { answer = value - 2; }
+    }
+  }
+
+  public class DeepThought : Does<RDeepThought> {
+    [Placeholder] public int Answer { 
+      get { return 4 + 2; } 
+      set { } 
+    }
+  }
+
+  public class DeepThoughtTest : DynamicTestFixture {
+    public override void Test() {
+      var thinker = new DeepThought();
+      thinker.Answer = 42;
+      Assert.AreEqual(42, thinker.Answer);
+    }
+  }
+
+  // TODO: possible bug: test superceded property
+
+  // TODO: auto-property
+
+  // TODO: placeholder in individual getter or setter
+
+}
