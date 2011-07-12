f7a1c90eeecd179d8d41ed11f267d6129d898222
Jordao <jordao@ymail.com>
2011-07-12 08:26:43 -0400

    Small refactorings on the conflict detection model classes

--- src/NRoles.Engine/ConflictDetection/ClassMember.cs
+++ src/NRoles.Engine/ConflictDetection/ClassMember.cs
@@ -16,15 +16,6 @@ namespace NRoles.Engine {
     public TypeReference Class { get { return Type; } }
 
     /// <summary>
-    /// Indicates if the represented member is inherited.
-    /// </summary>
-    /// <remarks>
-    /// If it's an inherited member, the <see cref="Class"/> property will be the 
-    /// parent class where the member is defined.
-    /// </remarks>
-    public bool IsInherited { get; private set; }
-
-    /// <summary>
     /// Creates a new instance of this class.
     /// </summary>
     /// <param name="class">The member class.</param>
@@ -37,13 +28,40 @@ namespace NRoles.Engine {
     }
 
     /// <summary>
+    /// Indicates if the represented member is inherited.
+    /// </summary>
+    /// <remarks>
+    /// If it's an inherited member, the <see cref="Class"/> property will be the 
+    /// parent class where the member is defined.
+    /// </remarks>
+    public bool IsInherited { get; private set; }
+
+    /// <summary>
     /// Indicates if the member is a placeholder.
     /// A placeholder member is meant to be replaced.
     /// </summary>
     public bool IsPlaceholder {
+      get { return Definition.IsPlaceholder(); }
+    }
+
+    /// <summary>
+    /// Indicates if the member is abstract.
+    /// </summary>
+    public override bool IsAbstract {
       get {
-        return Definition.IsPlaceholder();
+        // abstractedness is only applicable to method definitions
+        return
+          Definition is MethodDefinition &&
+          ((MethodDefinition)Definition).IsAbstract;
       }
+    }
+
+    /// <summary>
+    /// A foreign member is a member that comes from outside the <see cref="Type"/>.
+    /// <see cref="ClassMember"/>s are never foreign.
+    /// </summary>
+    public override bool IsForeign { 
+      get { return false; } 
     }
 
     public override void Process() {
@@ -96,14 +114,6 @@ namespace NRoles.Engine {
       memberGroup.MarkAsSuperceded(this);
       if (!member.IsSupersede()) {
         // TODO: add a warning?
-      }
-    }
-
-    public override bool IsForeign { get { return false; } }
-
-    public override bool IsAbstract {
-      get {
-        return Definition is MethodDefinition && ((MethodDefinition)Definition).IsAbstract; 
       }
     }
 
