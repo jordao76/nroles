c5c9c10f36a12c0c96aa664a4493a59bf29bdcf5
Jordao <jordao@ymail.com>
2011-07-13 17:52:47 -0400

    Group conflict resolver

---
+++ src/NRoles.Engine/ConflictDetection/GroupConflictResolver.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NRoles.Engine {
+
+  public class GroupConflictResolver {
+
+    public ConflictDetectionResult Process(ContributedConflictGroup group) {
+      group.ResolvedMember = null;
+      group.HasConflict = false;
+
+      var result = new ConflictDetectionResult();
+
+      if (group.IsSuperceded) {
+        return result; // TODO: process warnings? views here are superfluous since the member is overriden!
+      }
+
+      // to solve the conflict, EXACTLY one non-abstract member must remain in the list
+
+      // only consider foreign members
+      var resolvedMembers = group.Members.Where(roleMember => roleMember.IsForeign).ToList();
+
+      // process excluded members
+      resolvedMembers = resolvedMembers.Where(roleMember => !roleMember.IsExcluded).ToList();
+      if (resolvedMembers.Count == 0) {
+        result.AddMessage(Error.AllMembersExcluded(group.TargetType, group.ResolveRepresentation()));
+        return result;
+      }
+
+      // process aliased members
+      resolvedMembers = resolvedMembers.Where(roleMember => !roleMember.IsAliased).ToList();
+      if (resolvedMembers.Count == 0) {
+        // all members are aliased
+        group.ResolvedMember = null;
+        return result;
+      }
+
+      // process base methods
+      if (resolvedMembers.All(roleMember => roleMember.Definition.IsBaseMethod())) {
+        // all members are virtual base members, they'll be provided by the composing class
+        group.IsBaseMethod = true;
+        group.ResolvedMember = null;
+        return result;
+      }
+      Tracer.Assert(!resolvedMembers.Any(roleMember => roleMember.Definition.IsBaseMethod()), "Base methods cannot be provided by roles");
+
+      if (resolvedMembers.All(roleMember => roleMember.IsAbstract)) {
+        if (!group.TargetType.IsRole()) {
+          result.AddMessage(Error.DoesNotImplementAbstractRoleMember(group.TargetType, group.ResolveRepresentation()));
+        }
+        return result;
+      }
+
+      // process abstract members
+      resolvedMembers = resolvedMembers.Where(roleMember => !roleMember.IsAbstract).ToList();
+      if (resolvedMembers.Count > 1) {
+        group.HasConflict = true;
+        result.AddMessage(Error.Conflict(group.TargetType, group.ResolveRepresentation(), resolvedMembers));
+        return result;
+      }
+
+      group.ResolvedMember = resolvedMembers.Single();
+
+      return result;
+    }
+
+  }
+
+}
--- src/NRoles.Engine/Support/Tracer.cs
+++ src/NRoles.Engine/Support/Tracer.cs
@@ -12,8 +12,8 @@ namespace NRoles.Engine {
       Trace.WriteLineIf(MutatorSwitch.Level == TraceLevel.Verbose, string.Format(message, parameters));
     }
     [Conditional("TRACE")]
-    public static void Assert(bool condition = false) {
-      if (!condition) throw new InvalidProgramException();
+    public static void Assert(bool condition = false, string message = null) {
+      if (!condition) throw new InvalidProgramException(message ?? "Assert condition failed!");
     }
 
     // TODO: remove these methods?
