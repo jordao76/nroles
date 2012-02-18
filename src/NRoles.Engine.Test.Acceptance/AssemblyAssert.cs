b24d6b4497b98b7c8ac75c71d21bfe74efd7342e
Jordao <jordao@ymail.com>
2012-02-18 18:24:41 -0500

    PEVerify path in Windows XP

--- src/NRoles.Engine.Test.Acceptance/AssemblyAssert.cs
+++ src/NRoles.Engine.Test.Acceptance/AssemblyAssert.cs
@@ -8,11 +8,20 @@ namespace NRoles.Engine.Test {
   public static class AssemblyAssert {
 
     static string ResolvePEVerifyPath() {
+      // .NET 4.0
+      
       var peVerifyPath =
         Path.Combine(
           Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
           @"Microsoft SDKs\Windows\v7.1\Bin\NETFX 4.0 Tools\x64\PEVerify.exe"); // Windows 7
       if (File.Exists(peVerifyPath)) return peVerifyPath;
+
+      peVerifyPath =
+        Path.Combine(
+          Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
+          @"Microsoft SDKs\Windows\v7.0A\bin\NETFX 4.0 Tools\PEVerify.exe"); // Windows XP
+      if (File.Exists(peVerifyPath)) return peVerifyPath;
+      
       return null;
     }
 
